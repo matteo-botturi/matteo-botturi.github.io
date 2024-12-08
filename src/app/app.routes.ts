450dac83324df18bcdfa940bce5c7c99fa232435
Matteo <botturi.matteo@gmail.com>
2024-12-08 19:14:26 +0100

    Improved resizing hero image and prevent browser dark mode override

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,39 +9,19 @@ export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     {
         path: 'home',
-        component: HomeComponent,
-        data: {
-          title: 'Accueil - Mon Portfolio',
-          description: 'Bienvenue sur le portfolio de Matteo Botturi, un développeur Full Stack passionné par le développement web, l\'innovation et la création d\'interfaces intuitives.',
-          keywords: 'développeur, portfolio, compétences, projets, full stack, web'
-        }
+        component: HomeComponent
       },
       {
         path: 'about',
-        component: AboutComponent,
-        data: {
-          title: 'À propos de moi',
-          description: 'Découvrez le parcours professionnel et les compétences de Matteo Botturi, développeur Full Stack avec une expertise en technologies web et une passion pour l\'innovation.',
-          keywords: 'développeur, full stack, parcours, compétences, technologies web'
-        }
+        component: AboutComponent
       },
       {
         path: 'skills',
-        component: SkillsComponent,
-        data: {
-          title: 'Compétences - Mon Portfolio',
-          description: 'Explorez les compétences techniques de Matteo Botturi, de la programmation Front-End à la gestion des bases de données, en passant par le développement Full Stack et les technologies modernes.',
-          keywords: 'compétences, full stack, développement, front-end, back-end, technologies'
-        }
+        component: SkillsComponent
       },
       {
         path: 'projects',
-        component: ProjectsComponent,
-        data: {
-          title: 'Projets - Mon Portfolio',
-          description: 'Découvrez les projets réalisés par Matteo Botturi, de la conception de sites web aux applications dynamiques, en passant par des solutions innovantes adaptées aux besoins des clients.',
-          keywords: 'projets, développement web, applications, solutions innovantes, création de sites'
-        }
+        component: ProjectsComponent
       }, 
     { path: '**', component: NotFoundComponent },
 ];
